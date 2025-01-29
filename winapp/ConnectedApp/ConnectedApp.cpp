c3a25c0ea0fff0c7bbc15083cbfe8db6d4adfd0a
Omer Turgeman <omer10068@gmail.com>
2025-01-29 17:19:03 +0200

    main fixes. add try catch

--- winapp/ConnectedApp/ConnectedApp.cpp
+++ winapp/ConnectedApp/ConnectedApp.cpp
@@ -11,55 +11,23 @@
 std::mutex mtx;
 
 int main() {
-    CommonObjects common;
-    DrawThread draw;
-    auto draw_th = std::jthread([&] {draw(common); });
-    DownloadThread down;
-    auto down_th = std::jthread([&] {down(common); });
-    down.SetUrl("http://....");
-    std::cout << "running...\n";
-    /*down_th.join();
-    draw_th.join();*/
+    try {
+        CommonObjects common;
+        DrawThread draw;
+        auto draw_th = std::jthread([&] { draw(common); });
+        DownloadThread down;
+        auto down_th = std::jthread([&] { down(common); });
 
-	return 0;
+        down.SetUrl("http://....");
+        std::cout << "running...\n";
+
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception caught: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Unknown exception caught!" << std::endl;
+    }
+
+    return 0;
 }
-
-//int main() {
-//    CommonObjects common;
-//    DrawThread draw;
-//    DownloadThread down;
-//
-//    // Start the download thread
-//    auto down_th = std::jthread([&] { down(common); });
-//
-//    // Wait for the download thread to finish
-//    down_th.join();
-//
-//    // Check if data is ready
-//    if (common.data_ready) {
-//        std::cout << "Movies fetched successfully!\n";
-//        std::cout << "Number of movies: " << common.movies.size() << "\n\n";
-//
-//        // Print details of each movie
-//        for (const auto& movie : common.movies) {
-//            std::cout << "Title: " << movie.title << "\n";
-//            std::cout << "Overview: " << movie.overview << "\n";
-//            std::cout << "Release Date: " << movie.release_date << "\n";
-//            std::cout << "Popularity: " << movie.popularity << "\n";
-//            std::cout << "Vote Average: " << movie.vote_average << "\n";
-//            std::cout << "Vote Count: " << movie.vote_count << "\n";
-//            std::cout << "Poster Path: " << movie.poster_path << "\n";
-//            std::cout << "----------------------------------------\n";
-//        }
-//    }
-//    else {
-//        std::cout << "Failed to fetch movies or no data available.\n";
-//    }
-//
-//    // Start the draw thread (if needed)
-//    auto draw_th = std::jthread([&] { draw(common); });
-//    draw_th.join();
-//
-//    return 0;
-//}
-
